bffa8ce60db5097550b5014d9b0aee5bba786777
Julien Marrec <julien.marrec@gmail.com>
2024-01-24 10:22:50 +0100

    openstudio-specific patch: embedded gems (rb_hasFile / rb_require_embedded)

--- recipes/ruby/all/test_package/test_package.cpp
+++ recipes/ruby/all/test_package/test_package.cpp
@@ -1,4 +1,6 @@
 #include <ruby.h>
+#include <iostream>
+#include <string>
 
 // when --static-linked-ext is used, ruby defines EXTSTATIC as 1
 #if defined(EXTSTATIC) && EXTSTATIC
@@ -54,4 +56,20 @@ int main(int argc, char* argv[]) {
   ruby_finalize();
 
   return EXIT_SUCCESS;
+}
+
+extern "C"
+{
+  int rb_hasFile(const char* t_filename) {
+    // TODO Consider expanding this to use the path which we have artificially defined in embedded_help.rb
+    std::string expandedName = std::string(":/ruby/2.7.0/") + std::string(t_filename) + ".rb";
+    std::cout << "rb_hasFile called: " << expandedName << std::endl;
+    return 0;
+  }
+
+  int rb_require_embedded(const char* t_filename) {
+    std::string require_script = R"(require ')" + std::string(t_filename) + R"(')";
+    std::cout << "require_script called: " << require_script << std::endl;
+    return 0;
+  }
 }
